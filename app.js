b5671b776efecd6ab3e6b02b87121767b6282137
Bong <borobong230@gmail.com>
2023-10-12 22:52:32 +0900

    feat: Add jest environment and simple test code for js-spa

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
 window.addEventListener("load", () => {
-  console.log("test");
-
-  const app = document.getElementById("app");
-
-  const routes = {
-    home: "<h1>홈 페이지</h1>",
-    about: "<h1>소개 페이지</h1>",
-    contact: "<h1>연락처 페이지</h1>",
-  };
-
-  const render = (route) => {
-    console.log("route", route);
-
-    app.innerHTML = routes[route] || "<h1>404 페이지를 찾을 수 없습니다.</h1>";
-  };
-
   window.addEventListener("hashchange", () => {
     const route = location.hash.slice(1);
     render(route);
@@ -27,3 +11,30 @@ window.addEventListener("load", () => {
     render(route);
   }
 });
+
+// const render = (route) => {
+//   const app = document.getElementById("app");
+
+//   const routes = {
+//     home: "<h1>홈 페이지</h1>",
+//     about: "<h1>소개 페이지</h1>",
+//     contact: "<h1>연락처 페이지</h1>",
+//   };
+
+//   app.innerHTML = routes[route] || "<h1>404 페이지를 찾을 수 없습니다.</h1>";
+// };
+
+function render(page) {
+  const app = document.getElementById("app");
+  if (page === "home") {
+    app.innerHTML = "<h1>홈 페이지</h1>";
+  } else if (page === "about") {
+    app.innerHTML = "<h1>소개 페이지</h1>";
+  } else if (page === "contact") {
+    app.innerHTML = "<h1>연락처 페이지</h1>";
+  } else {
+    app.innerHTML = "<h1>404 페이지를 찾을 수 없습니다.</h1>";
+  }
+}
+
+export { render };
