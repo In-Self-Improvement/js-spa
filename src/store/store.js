b9c1af06602a7049b4812642e084433258160459
Bong <borobong230@gmail.com>
2024-12-24 21:09:39 +0900

    :sparkles: [Feat] feat: Implement a reactive Store class with commit and dispatch methods for state management #master

---
+++ src/store/store.js
@@ -0,0 +1,39 @@
+export class Store {
+  #state;
+  #mutations;
+  #actions;
+  state = {};
+
+  constructor({ state, mutations = {}, actions = {} }) {
+    this.#state = observable(state);
+    this.#mutations = mutations;
+    this.#actions = actions;
+
+    Object.keys(state).forEach((key) => {
+      Object.defineProperty(this.state, key, { get: () => this.#state[key] });
+    });
+  }
+
+  commit(type, payload) {
+    const mutation = this.#mutations[type];
+    if (!mutation) {
+      throw new Error(`Unknown mutation type: ${type}`);
+    }
+    mutation(this.#state, payload);
+  }
+
+  dispatch(type, payload) {
+    const action = this.#actions[type];
+    if (!action) {
+      throw new Error(`Unknown action type: ${type}`);
+    }
+    return action(
+      {
+        state: this.#state,
+        commit: this.commit.bind(this),
+        dispatch: this.dispatch.bind(this),
+      },
+      payload
+    );
+  }
+}
