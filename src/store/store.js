c5ca02584055384cd34b3772d420778e00515417
Bong <borobong230@gmail.com>
2024-12-25 22:56:44 +0900

    :hammer: [Update] store.js 업데이트 #master

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
+import { observable } from "./observer.js";
 export class Store {
   #state;
   #mutations;
   #actions;
-  state = {};
 
   constructor({ state, mutations = {}, actions = {} }) {
     this.#state = observable(state);
@@ -10,7 +10,10 @@ export class Store {
     this.#actions = actions;
 
     Object.keys(state).forEach((key) => {
-      Object.defineProperty(this.state, key, { get: () => this.#state[key] });
+      Object.defineProperty(this.state, key, {
+        get: () => this.#state[key],
+        enumerable: true,
+      });
     });
   }
 
